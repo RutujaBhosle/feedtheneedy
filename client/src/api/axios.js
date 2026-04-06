4f1c0bc81921b67c332e418569edbe3d12b04fcd
RutujaBhosle <bhoslerutujar@gmail.com>
2026-04-06 11:56:20 +0530

    fix backend url

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,6 @@
 import axios from 'axios'
-
 const API = axios.create({
-  baseURL: 'https://feedtheneedy-backend.onrender.com/api'
+  baseURL: 'https://feedtheneedy-jrlo.onrender.com/api'
 })
 
 API.interceptors.request.use((req) => {
